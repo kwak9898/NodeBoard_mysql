7466b387918e8804eff1b1fcc4cb5129afa93ae3
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-01-16 16:55:16 +0900

    회원가입, 로그인 완료

--- node_board/controller/user.js
+++ node_board/controller/user.js
@@ -44,6 +44,38 @@ const createUser = async (req, res, next) => {
     }
 }
 
+const postLoginSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{4,30}$")).required(),
+});
+
+const loginUser = async (req, res, next) => {
+    try {
+        const { username, password } = await postLoginSchema.validateAsync(req.body)
+
+        const user = await Users.findOne({ where: { userName: username, passWord: password } })
+
+        if (!user) {
+            res.status(400).send({
+                errorMessage: "아이디 또는 패스워드가 잘못됐습니다."
+            })
+            return
+        }
+
+        const token = jwt.sign({ userName: user.userName }, "my-token")
+        res.send({
+            token
+        })
+    } catch (error) {
+        console.log('-------------------------------------')
+        console.log('에러발생:' + error)
+        res.status(400).send({
+            errorMessage: "요청한 형식이 올바르지 않습니다."
+        })
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser
 }
--- node_board/routers/user.js
+++ node_board/routers/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const userControll = require('../controller/user')
-const authmiddleware = require('../middlewares/auth-middleware')
+const authMiddleware = require('../middlewares/auth-middleware')
 const router = express.Router()
 
 router.post('/', userControll.createUser)
+router.post('/login', userControll.loginUser)
 
 module.exports = router
